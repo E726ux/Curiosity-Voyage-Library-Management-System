36342e8dec90a9b228b89ba3348a48429e9adfe7
Ethan Ryder <eryder1@buffs.wtamu.edu>
2024-12-10 13:54:17 -0600

    New Changes

--- Models/DbContext.cs
+++ Models/DbContext.cs
@@ -1,3 +1,16 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyModel;
 
 namespace Curiosity_Voyage_Library_Management_System_1.Models;
+
+public class LibraryDbContext : DbContext
+{
+public LibraryDbContext(DbContextOptions<LibraryDbContext> options)
+                : base(options)
+                {
+
+                }  
+
+    public DbSet<Library>? Books {get; set;} 
+}
+
